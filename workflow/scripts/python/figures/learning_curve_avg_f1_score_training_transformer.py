ee45c95b8d8f864e0cf7a654a711474e7c590e89
Etienne <etienne@UN05FMSS009020.2>
2023-09-15 11:17:08 -0400

    Fscore learning curves transformer

---
+++ workflow/scripts/python/figures/learning_curve_avg_f1_score_training_transformer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+import functions as ft 
+import random
+
+num_epoch = int(snakemake.params.num_epoch)
+dfs = []
+val = [random.uniform(0, 0.4) for _ in range(10)]
+
+print(snakemake.input.f1_score_tsv)
+for i, p in enumerate(snakemake.input.f1_score_tsv):
+    df = pd.read_csv(p, sep='\t')
+    new_row = pd.DataFrame({'f1_score_per_epoch': [val[i]]})  # before first epoch (have to change it for real predicted value before training)
+    df = pd.concat([new_row, df])
+    df['epoch'] = [i for i in range(0, num_epoch+1)]
+    dfs.append(df)
+
+f1_score_df = pd.concat(dfs)
+
+# Grouby the df by epoch and compute mean and stdev
+mean = f1_score_df.groupby('epoch').mean().reset_index().rename(columns={'f1_score_per_epoch': 'avg_f1_score'})
+std = f1_score_df.groupby('epoch').std().reset_index().rename(columns={'f1_score_per_epoch': 'std_f1_score'})
+merged_df = mean.merge(std, how='left', on='epoch')
+
+# Create lineplot of average f1_score across folds and epochs +/- stdev across folds
+ft.lineplot_errorbars(merged_df, 'epoch', 'avg_f1_score', 'std_f1_score', None, 'Number of epoch', 'Average F1 score across folds', 
+                'Average F1 score across folds and epochs', 'grey', snakemake.output.learning_curve)
