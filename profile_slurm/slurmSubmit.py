2297e84e7abb71920ebf14900658acc91b29906a
Etienne <etienne@UN05FMSS009020.2>
2024-03-28 15:12:50 -0400

    slurmsubmit modify time depending on size of chr in job

--- profile_slurm/slurmSubmit.py
+++ profile_slurm/slurmSubmit.py
@@ -17,6 +17,16 @@ cmdline = "sbatch "
 for param, val in job_properties['cluster'].items():
     cmdline += "--{param} {val} ".format(param=param, val=val)
 
+# Check if the rule name is 'genome_windows_separate_chrom'
+#if job_properties["rule"] == "genome_windows_separate_chrom":
+#
+#    # Update the cluster properties with the new time limit
+#    inputs = job_properties.get("input", [])
+#    chr_path = [i for i in inputs if i.endswith('.fa') | i.endswith('.fasta')][0]
+#    chr_size = os.path.getsize(chr_path) # in bytes
+#    job_properties['cluster']['time'] = time_limit(chr_size)
+
+
 # Set up dependencies
 dependencies = set(sys.argv[1:-1])
 if dependencies:
